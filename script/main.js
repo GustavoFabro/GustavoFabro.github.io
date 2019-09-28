1f54a2eb78638cbe77c6299c230e55faa32628bf
Gustavo <gustavofabro.f@gmail.com>
2019-09-28 15:59:23 -0300

    menu

--- script/main.js
+++ script/main.js
@@ -1,11 +1,18 @@
 $(document).ready(onPageReady);
 
 function onPageReady() {
-    $('.btn-view').click(e => {
-        let target = $(e.target).attr('data-for');
+    $('.mobile-toggle-menu').on('click', e => {
+        $('.mobile-menu').toggleClass('selected')
+    });
+
+    $('.menu-item, .menu-mobile-item').on('click', e => {
+        const target = e.target.hasAttribute('data-for')? e.target : e.target.parentElement
+        const targetDataFor = $(target).attr('data-for')
 
         $('[id^="content"]').hide()
+        $('.selected').toggleClass('selected')
 
-        $(`[id="content-${target}"]`).fadeTo("slow", 1);
+        $(`[id="content-${targetDataFor}"]`).fadeTo("slow", 1)
+        $(target).toggleClass('selected')
     })
 }
