2020c00a96a6191d995d1f4babc72c8e36a31387
gustavofabro <gustavofabro.f@gmail.com>
2020-02-08 11:59:16 -0300

    Stargin new layout

--- script/main.js
+++ script/main.js
@@ -1,18 +1,74 @@
 $(document).ready(onPageReady);
 
 function onPageReady() {
-    $('.mobile-toggle-menu').on('click', e => {
-        $('.mobile-menu').toggleClass('selected')
+  $(".mobile-toggle-menu").on("click", e => {
+    $(".mobile-menu").toggleClass("selected");
+  });
+
+  $(".menu-item, .menu-mobile-item").on("click", e => {
+    const target = e.target.hasAttribute("data-for")
+      ? e.target
+      : e.target.parentElement;
+
+    setSection(target);
+  });
+
+  function setSection(target) {
+    const targetDataFor = $(target).attr("data-for");
+
+    $('[id^="content"]').hide();
+    $(".selected").toggleClass("selected");
+
+    $(`[id="content-${targetDataFor}"]`).fadeTo("slow", 1);
+
+    $(target).toggleClass("selected");
+  }
+
+  function configureSection() {
+    const sessaoIntro = $('[data-for="intro"]')[0];
+
+    setSection(sessaoIntro);
+  }
+
+  async function configureText() {
+    function getYearsWorking() {
+      return 3;
+    }
+
+    function splep(mili) {
+      return new Promise(resolve => setTimeout(() => resolve(), mili));
+    }
+
+    const textIntro = `Hi, I'm Gustavo, full stack developer with ${getYearsWorking()} years of experience in the field. Bachelor in computer science from UNESC (2014 - 2018).`;
+
+    const element = $("#content-intro p")[0];
+    const textIntroParts = textIntro.split("");
+
+    for (let index = 0; index < textIntroParts.length; index++) {
+      const text = textIntroParts[index];
+
+      await splep(20);
+      $(element).append(text);
+    }
+  }
+
+  function configureTerminal() {
+    const terminalElement = $("#terminal");
+
+    terminalElement.keydown(e => {
+      if (e.keyCode == 13) {
+        const jsOutput = eval(terminalElement.val());
+
+        $("#output-terminal").text(jsOutput);
+      }
     });
+  }
 
-    $('.menu-item, .menu-mobile-item').on('click', e => {
-        const target = e.target.hasAttribute('data-for')? e.target : e.target.parentElement
-        const targetDataFor = $(target).attr('data-for')
+  function init() {
+    configureSection();
+    configureText();
+    configureTerminal();
+  }
 
-        $('[id^="content"]').hide()
-        $('.selected').toggleClass('selected')
-
-        $(`[id="content-${targetDataFor}"]`).fadeTo("slow", 1)
-        $(target).toggleClass('selected')
-    })
+  init();
 }
